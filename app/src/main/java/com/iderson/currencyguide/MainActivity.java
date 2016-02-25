1c4c043d20a79220c5b003479bb093edc171643f
Andrew Shershun <andreadson90@gmail.com>
2016-02-25 20:50:59 +0200

    fragments and search update

--- app/src/main/java/com/iderson/currencyguide/MainActivity.java
+++ app/src/main/java/com/iderson/currencyguide/MainActivity.java
@@ -5,20 +5,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
@@ -29,19 +22,15 @@ import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.iderson.currencyguide.adapter.RVOrgAdapter;
-import com.iderson.currencyguide.database.CurrencyContentProvider;
-import com.iderson.currencyguide.database.CurrencyDBHelper;
+import com.iderson.currencyguide.fragments.GuideFragment;
 import com.iderson.currencyguide.models.OrganizationModel;
-import com.iderson.currencyguide.service.Helper;
 import com.iderson.currencyguide.service.LoadCompleteReceiver;
 import com.iderson.currencyguide.service.LoadService;
 import com.iderson.currencyguide.service.ServiceStarter;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity
-        implements LoaderManager.LoaderCallbacks<Cursor> {
+public class MainActivity extends AppCompatActivity {
 
     public final static String TAG = "Logs";
     private static final int MENU_MULTI_WINDOW = 0;
@@ -56,6 +45,7 @@ public class MainActivity extends AppCompatActivity
     private SharedPreferences           mPrefs;
     private ServiceStarter              mAlarm = null;
     private boolean                     doubleBackToExitPressedOnce = false;
+    private String DATA = "SEARCH";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,10 +57,23 @@ public class MainActivity extends AppCompatActivity
             finish();
 
         initUI();
+        startFragment("");
+    }
+
+    public void startFragment(String _query) {
+        GuideFragment fragment;
+        fragment = new GuideFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(DATA, _query);
+        fragment.setArguments(bundle);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, fragment).commit();
     }
 
     private void initUI() {
-        Helper.admobLoader(this, getResources(), findViewById(R.id.adView));
+//        Helper.admobLoader(this, getResources(), findViewById(R.id.adView));
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -78,13 +81,6 @@ public class MainActivity extends AppCompatActivity
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             getWindow().setStatusBarColor(getResources().getColor(R.color.dark_primary));
         }
-        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swpRefreshLayout_AM);
-        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                updateList();
-            }
-        });
 
         mPrefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
         boolean firstStart = mPrefs.getBoolean("firstStart", true);
@@ -106,45 +102,8 @@ public class MainActivity extends AppCompatActivity
                 LoadService.ACTION_MYINTENTSERVICE);
         intentFilter.addCategory(Intent.CATEGORY_DEFAULT);
         registerReceiver(myBroadcastReceiver, intentFilter);
-        //get data from database
-        getSupportLoaderManager().initLoader(0, null, this);
 
-    }
 
-    private void updateList() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                MainActivity.this.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if(Helper.isOnline(MainActivity.this, false, true)) {
-                            Intent intent = new Intent(MainActivity.this, LoadService.class);
-                            startService(intent);
-                        }
-//                        initUI();
-                        mSwipeRefreshLayout.setRefreshing(false);
-                    }
-                });
-            }
-        }).start();
-    }
-
-    private void populateRV(List<OrganizationModel> _list) {
-        if (mRvAdapter != null) mRvAdapter.notifyDataSetChanged();
-        else {
-            LinearLayoutManager llm = new LinearLayoutManager(MainActivity.this);
-            mRvBanks = (RecyclerView) findViewById(R.id.rvBanks_AM);
-            mRvBanks.setLayoutManager(llm);
-            mRvBanks.setItemAnimator(new DefaultItemAnimator());
-        }
-        mRvAdapter = new RVOrgAdapter(MainActivity.this, _list);
-        mRvBanks.setAdapter(mRvAdapter);
     }
 
     /*
@@ -156,10 +115,23 @@ public class MainActivity extends AppCompatActivity
         inflater.inflate(R.menu.menu_main, menu);
         SearchManager searchManager =
                 (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView =
+        final SearchView searchView =
                 (SearchView) menu.findItem(R.id.action_search).getActionView();
         searchView.setSearchableInfo(
                 searchManager.getSearchableInfo(getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchView.clearFocus();
+                startFragment(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
 
         return super.onCreateOptionsMenu(menu);
     }
@@ -186,72 +158,6 @@ public class MainActivity extends AppCompatActivity
         return super.onPrepareOptionsMenu(menu);
     }
 
-    /*
-    * Prepere handling database operation
-    */
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        Uri uri = CurrencyContentProvider.CONTENT_URI;
-        return new CursorLoader(this, uri, null, null, null, null);
-    }
-
-    /*
-    * Database listener to populate currency list
-    */
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        String id = "";
-        int count = 0;
-        String title = "";
-        String region = "";
-        String city = "";
-        String phone = "";
-        String address = "";
-        String link = "";
-
-        if (data == null) {
-            count = 0;
-        } else {
-            count = data.getCount();
-            data.moveToFirst();
-            mBankList = new ArrayList<>();
-            for (int i = 0; i < count; i++) {
-                id = data.getString(data.getColumnIndex(CurrencyDBHelper.FIELD_ROW_ID));
-                title = data.getString(data.getColumnIndex(CurrencyDBHelper.FIELD_TITLE));
-                region = data.getString(data.getColumnIndex(CurrencyDBHelper.FIELD_REGION));
-                city = data.getString(data.getColumnIndex(CurrencyDBHelper.FIELD_CITY));
-                phone = data.getString(data.getColumnIndex(CurrencyDBHelper.FIELD_PHONE));
-                address = data.getString(data.getColumnIndex(CurrencyDBHelper.FIELD_ADDRESS));
-                link = "" + data.getString(data.getColumnIndex(CurrencyDBHelper.FIELD_LINK));
-                if((!mQueryStr.equals("") &&
-                        (title.toLowerCase().contains(mQueryStr.toLowerCase().trim()) ||
-                                city.toLowerCase().contains(mQueryStr.toLowerCase().trim()) ||
-                                region.toLowerCase().contains(mQueryStr.toLowerCase().trim())))
-                        || mQueryStr.equals(""))
-                mBankList.add(new OrganizationModel(
-                        id,
-                        title,
-                        region,
-                        city,
-                        phone,
-                        address,
-                        link));
-                data.moveToNext();
-            }
-            populateRV(mBankList);
-        }
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-
-    }
-
     @Override
     public void onBackPressed() {
         if (doubleBackToExitPressedOnce) {
@@ -271,7 +177,7 @@ public class MainActivity extends AppCompatActivity
 
             @Override
             public void run() {
-                doubleBackToExitPressedOnce=false;
+                doubleBackToExitPressedOnce = false;
             }
         }, 2000);
     }
